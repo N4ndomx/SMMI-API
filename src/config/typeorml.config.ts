cfc5f313a805eccf1982d64610f50d25d3800b76
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2024-04-24 01:49:08 -0600

    models db

--- src/config/typeorml.config.ts
+++ src/config/typeorml.config.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { EnfermeraModel } from 'src/enfermeras/infra/persistencia/models/enfermera.model';
-import { EspecialidadModel } from 'src/especialidades/infra/persistencia/models/especialidades.model';
-import { Medico_Especialidad_Model } from 'src/especialidades/infra/persistencia/models/medico-especialidad.model';
-import { MedicoModel } from 'src/medicos/infra/persistencia/models/medico.model';
+import { CatalogoSensoresModel, EmpleadoModel, EnfermeraModel, EspecialidadesModule, HabitacionModel, MedicoModel, Medico_Especialidad_Model, SensorDataModel } from 'src/shared/models';
+
 
 @Injectable()
 export class TypeOrmConfig implements TypeOrmOptionsFactory {
@@ -15,7 +13,7 @@ export class TypeOrmConfig implements TypeOrmOptionsFactory {
       database: process.env.DATABASE_NAME,
       password: process.env.DATABASE_PASSWORD,
       username: process.env.DATABASE_USERNAME,
-      entities: [MedicoModel, EnfermeraModel, EspecialidadModel, Medico_Especialidad_Model],
+      entities: [CatalogoSensoresModel, EmpleadoModel, EnfermeraModel, EspecialidadesModule, HabitacionModel, MedicoModel, Medico_Especialidad_Model, SensorDataModel],
       autoLoadEntities: true,
       extra: {
         ssl: true,
---
+++ src/shared/models/index.ts
@@ -0,0 +1,8 @@
+export { EnfermeraModel } from "src/enfermeras/infra/persistencia/models/enfermera.model";
+export { EspecialidadesModule } from "src/especialidades/especialidades.module";
+export { Medico_Especialidad_Model } from "src/especialidades/infra/persistencia/models/medico-especialidad.model";
+export { HabitacionModel } from "src/habitaciones/infra/persistencia/models/habitaciones.model";
+export { MedicoModel } from "src/medicos/infra/persistencia/models/medico.model";
+export { CatalogoSensoresModel } from "src/sensores/infra/persistencia/models/catalogo-sensores.model";
+export { SensorDataModel } from "src/sensores/infra/persistencia/models/sensor.model";
+export { EmpleadoModel } from "./empleado.model";
