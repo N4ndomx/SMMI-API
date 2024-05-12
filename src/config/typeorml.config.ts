b38e765019b22f9e72a100f6d0e69dbb82ac7bda
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2024-05-11 18:54:27 -0600

    conexion bb Neon

--- src/config/typeorml.config.ts
+++ src/config/typeorml.config.ts
@@ -14,7 +14,7 @@ export class TypeOrmConfig implements TypeOrmOptionsFactory {
       username: process.env.DATABASE_USERNAME,
       autoLoadEntities: true,
       extra: {
-        ssl: false,
+        ssl: true,
         sslmode: 'require',
       },
       synchronize: true,
